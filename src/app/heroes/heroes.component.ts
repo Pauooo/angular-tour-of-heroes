4ff32009687673da323d3bdef6acad467156c167
Pauooo <ballotp@gmail.com>
2019-07-19 13:44:41 +0200

    Routing done

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-import { HeroService } from '../hero.service'
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -10,16 +9,12 @@ import { HeroService } from '../hero.service'
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  constructor( private heroService : HeroService ) { }
+  heroes: Hero[];
 
-  hero : Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heroes : Hero[];
-  selectedHero: Hero;
+  constructor( private heroService : HeroService ) { }
 
   ngOnInit() {
     this.getHeroes();
@@ -28,10 +23,6 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
   
